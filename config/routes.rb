6f56f9f1fbc1a458b8700118dd29690b7edfd6c7
Ricki DiCristoforo <edicrist@tulane.edu>
2017-11-02 17:00:49 +0000

    IT WORKS

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   mount Thredded::Engine => '/forum'
+  
+  get "/:advice" => "advice#show"
+  
+  
 end
