e62a0e5d94e4a6dc903c0dcd5a765c2ee8b91f0d
Ricki DiCristoforo <edicrist@tulane.edu>
2017-11-09 14:53:55 +0000

    fixed bugs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,7 @@ address: 'smtp.gmail.com',
 port: 587,
 domain: 'gmail.com',
 user_name: 'extinguishcontact@gmail.com',
-password: 'socialsecuritysystem',
+password: 'Ricki123',
 authentication: 'plain',
 enable_starttls_auto: true 
 }
