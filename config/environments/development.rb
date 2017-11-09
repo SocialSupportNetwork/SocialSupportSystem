6fdbb4bfe5c5dfe4cb6041352964acb653be50df
Ricki DiCristoforo <edicrist@tulane.edu>
2017-11-09 14:55:45 +0000

    fixed log in error on contact us page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,7 @@ address: 'smtp.gmail.com',
 port: 587,
 domain: 'gmail.com',
 user_name: 'extinguishcontact@gmail.com',
-password: 'Ricki123',
+password: 'ricki123',
 authentication: 'plain',
 enable_starttls_auto: true 
 }
