dbf6b6b7554dcf5fa4bfcff8f9a09e136d894904
Ricki DiCristoforo <edicrist@tulane.edu>
2017-12-13 19:07:00 +0000

    added boolean value 'canTalk' to users table, added method blockUsre to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
-#Will work on
+
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
+    end
+    
+    def blockUser
+       @user.canTalk = false 
     end
 end
