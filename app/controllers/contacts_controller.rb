ef2f1df80b0e0e402aa4b86604ac7b2bdb8a25bc
Billy Groble <wgroble@tulane.edu>
2017-12-15 23:39:50 +0000

    Fixed tips and made general layout fixes Tips table now uses percentage values headings and layouts standardized across pages tips and contacts redirect to home page after submission instead of blank intermediate page still display apropriate flash notification

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,8 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      redirect_to "/", alert: "Thank you for your message. We will contact you soon!"
+      # flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
     else
       flash.now[:error] = 'Cannot send message.'
       render :new
