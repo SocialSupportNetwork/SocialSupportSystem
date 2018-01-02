f888da2b43e542b48adebe75decaf6e8fce2f205
Billy Groble <wgroble@tulane.edu>
2018-01-02 16:32:48 +0000

    layout changes on header

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,16 @@
 admin = User.create!(
     username: "Admin",
     password: "123456",
-    admin: true
+    admin: true,
+    minimum_age: true,
+    terms_of_service: true,
+    screening: "Administrator Account"
 )
 
 messageboard = Thredded::Messageboard.create!(
     name: 'General',
     slug: 'general',
-    description: 'Determine severity of specific situations by polling our community'
+    description: 'Determine the severity of specific situations by polling our community.'
 )
 
 Thredded::Messageboard.create!(
@@ -20,6 +23,11 @@ Thredded::Messageboard.create!(
 Thredded::Messageboard.create!(
     name: 'Second Opinions',
     slug: 'second-opinions',
-    description: 'Has something felt different? Are you unsure about something that happned? Post here to just get a second (or third!) opinion from our community.'
-).save
+    description: 'Has something felt different? Are you unsure about something that happened? Post here to just get a second (or third!) opinion from our community.'
+)
 
+Thredded::Messageboard.create!(
+    name: 'Interesting Articles',
+    slug: 'interesting-articles',
+    description: "Have you come across any interesting articles that you'd like to share with the community? Post them here!"
+).save
