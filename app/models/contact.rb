415c45c3eb1c9f97eb8c66b5c2de8aa0660a0a4b
Billy Groble <wgroble@tulane.edu>
2018-01-04 03:14:58 +0000

    added user link back

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < MailForm::Base
+  
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
@@ -7,7 +8,7 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "Extinguish Form",
+      :subject => "Extinguish Contact",
       :to => "extinguishcontact@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
